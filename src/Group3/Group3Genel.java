77688f13e767af5243a5c06bcc96219c00a52a53
Hasan GULTEKIN <av.gultekinhasan@gmail.com>
2024-01-08 20:35:49 +0000

    Hasan file olusturuldu ve Group3Genel'de 6.satır degistirildi

--- src/Group3/Group3Genel.java
+++ src/Group3/Group3Genel.java
@@ -3,7 +3,7 @@ package Group3;
 public class Group3Genel {
 
     public static void main(String[] args) {
-        System.out.println("TEST1");
+        System.out.println("TEST1  -  Hasan Branch....");
         System.out.println("TEST2");
         System.out.println("TEST3");
     }
