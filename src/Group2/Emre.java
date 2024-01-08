94a7ac10f578d04391cdfdafe5bc5cf6d1c52a29
Emre VARLI <varliemre02@gmail.com>
2024-01-08 21:39:40 +0100

    Emre isimli class eklendi ve test1 de degisiklik yapildi.

---
+++ src/Group2/Emre.java
@@ -0,0 +1,10 @@
+package Group2;
+
+public class Emre {
+    public static void main(String[] args) {
+
+        System.out.println("Emre test1 ekledi");
+        System.out.println("Emre test2 ekledi");
+        System.out.println("Emre test3 ekledi");
+    }
+}
