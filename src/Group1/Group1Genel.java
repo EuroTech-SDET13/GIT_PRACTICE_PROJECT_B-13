01e826d2e18f1320faec1ef562b57a2905feb2ac
Mujdat Goksen <mujdatgoksen@gmail.com>
2024-01-08 21:37:07 +0100

    mujdat test1 degistirdi

--- src/Group1/Group1Genel.java
+++ src/Group1/Group1Genel.java
@@ -3,7 +3,7 @@ package Group1;
 public class Group1Genel {
 
     public static void main(String[] args) {
-        System.out.println("TEST1");
+        System.out.println("Mujdat Degistirdi");
         System.out.println("TEST2");
         System.out.println("TEST3");
     }
---
+++ src/Group1/Mujdat.java
@@ -0,0 +1,10 @@
+package Group1;
+
+public class Mujdat {
+    public static void main(String[] args) {
+        System.out.println("Test 1");
+
+
+    }
+
+}
