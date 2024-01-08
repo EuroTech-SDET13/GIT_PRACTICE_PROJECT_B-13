b496b0a653c3d3887de87c3b48d1e1e0f389089e
Talha OZLEBLEBICI <talha43gs@gmail.com>
2024-01-08 18:53:58 +0000

    Tests uploaded

--- src/Sample/GroupSampleGenel.java
+++ src/Sample/GroupSampleGenel.java
@@ -1,4 +1,10 @@
 package Sample;
 
 public class GroupSampleGenel {
+    public static void main(String[] args) {
+        String test="TEST";
+        System.out.println("test = " + test);
+        System.out.println("Test5");
+        System.out.println("Test6");
+    }
 }
