ae6d23c2d7239030308002818287c34308df7c61
Dilek Tuncay Aba <dilekaba.d@gmail.com>
2024-01-08 21:50:18 +0100

    yeni desisiklikler yapildi

---
+++ src/Group2/Dilek3.java
@@ -0,0 +1,9 @@
+package Group2;
+
+public class Dilek3 {
+    public static void main(String[] args) {
+        System.out.println("Test1");
+        System.out.println("Test1");
+        System.out.println("Test1");
+    }
+}
