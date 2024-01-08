f53dbb9fd40a03f9e7c1c4c4d623065547ddaee5
SukruYuksel <yksl3743dc@gmail.com>
2024-01-08 21:35:46 +0100

    Sükrü file olusturuldu Grup g3 Genelde 6. Satir degistirildi

--- src/Group3/Group3Genel.java
+++ src/Group3/Group3Genel.java
@@ -3,6 +3,7 @@ package Group3;
 public class Group3Genel {
 
     public static void main(String[] args) {
+        System.out.println("TEST1 - Sükrü Test");
         System.out.println("TEST1");
         System.out.println("TEST2");
         System.out.println("TEST3");
---
+++ src/Group3/Sukru.java
@@ -0,0 +1,9 @@
+package Group3;
+
+public class Sukru {
+
+    public static void main(String[] args) {
+        System.out.println("Sükrü Branch Test1 linie1");
+        System.out.println("Sükrü Branch Test2 linie2");
+    }
+}
